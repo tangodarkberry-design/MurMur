e93ccceac4fbc7fe8c84dfdb3146f8feff521bb4
tangodarkberry-design <tango.darkberry@gmail.com>
2026-09-20 15:52:22 +0100

    Fixed Stuff

--- Client/js/app.js
+++ Client/js/app.js
@@ -3,7 +3,7 @@
    ========================================================= */
 
 const MURMUR_SERVER_URL =
-    "https://randy-bill-charter-workshop.trycloudflare.com";
+    "https://introduce-attacks-involving-tiny.trycloudflare.com";
 
 const SERVER_URL =
     MURMUR_SERVER_URL;
