40f408839dbf608a2f55775c84a7045a946e1570
tangodarkberry-design <tango.darkberry@gmail.com>
2026-09-25 18:49:34 +0100

    NIGG

--- Client/js/app.js
+++ Client/js/app.js
@@ -3,7 +3,7 @@
    ========================================================= */
 
 const MURMUR_SERVER_URL =
-    "https://picked-suggested-prisoner-yard.trycloudflare.com";
+    " https://shaw-genius-ctrl-men.trycloudflare.com";
 
 let redirecting = false;
 
